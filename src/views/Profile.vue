4866f261d5cb73ce909ee94c5e53a220b752be86
HenriqueFutema <minifut0407@gmail.com>
2019-07-21 01:21:10 -0300

    Rediricionamento para o Login

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,21 @@
 <template>
-    <h1>Profile</h1>
+  <h1>Profile {{isLogged}}</h1>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-
-}
+  computed: mapState(["isLogged"]),
+  data() {
+    return {};
+  },
+  created: function() {
+    if (!this.isLogged) {
+      this.$router.push({ name: "home", query: { redirect: "/" } });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
