10de24b8f0c4db0e85442aa1ce09014da0db61b6
HenriqueFutema <minifut0407@gmail.com>
2019-07-22 14:46:11 -0300

    Get User no profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,18 +4,27 @@
 
 <script>
 import { mapState, mapGetters } from "vuex";
+import api from "../services/api";
+
 export default {
   computed: {
     ...mapState(["isLogged"]),
     ...mapGetters(["getIdUser", "getTokenUser"])
   },
   data() {
-    return {};
+    return {
+      user: {}
+    };
   },
-  created: function() {
+  created: async function() {
     if (!this.isLogged) {
       this.$router.push({ name: "home", query: { redirect: "/" } });
     }
+
+    const response = await api.get(`users/${this.getIdUser}`, {
+      headers: { Authorization: "Bearer " + this.getTokenUser }
+    });
+    this.user = response.data;
   }
 };
 </script>
