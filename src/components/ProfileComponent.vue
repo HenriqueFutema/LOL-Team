e5399b4a33fb5fb290eaecb4a351597caf4948bf
HenriqueFutema <minifut0407@gmail.com>
2019-07-22 21:23:38 -0300

    ProfileComponent show user

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -3,8 +3,9 @@
     <v-layout text-xs-center wrap>
       <v-flex xs12>
         <h1 class="display-3 font-weight-thin">Profile</h1>
-        <h3 class="display-3 font-weight-thin">{{user.email}}</h3>
-        <h3 class="display-3 font-weight-thin">Test</h3>
+        <h3 class="display-6 font-weight-thin">{{user.name}}</h3>
+        <h3 class="display-6 font-weight-thin">{{user.email}}</h3>
+        <h3 class="display-6 font-weight-thin">{{user.description}}</h3>
       </v-flex>
     </v-layout>
   </v-container>
