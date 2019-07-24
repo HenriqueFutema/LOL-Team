32944fab9aa82019007cdeb26d293064d9e8ff91
HenriqueFutema <minifut0407@gmail.com>
2019-07-24 00:16:06 -0300

    Requisição dos usuários

--- src/components/PlayersComponent.vue
+++ src/components/PlayersComponent.vue
@@ -3,18 +3,32 @@
     <v-layout text-xs-center wrap>
       <v-flex xs12>
         <h1 class="display-3 font-weight-thin">LOL Team</h1>
-
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {
+import api from "../services/api";
+import { mapGetters } from "vuex";
 
-}
+export default {
+  computed: {
+    ...mapGetters(["getTokenUser"])
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  created: async function() {
+    this.users = await api.get("users", {
+      headers: { Authorization: "Bearer " + this.getTokenUser }
+    });
+    console.log(this.users.data.docs);
+  }
+};
 </script>
 
 <style>
-
 </style>
