21d5d52342ca1d6d77db0f4e32ddf7cf6994dfb4
HenriqueFutema <minifut0407@gmail.com>
2019-07-30 06:09:10 -0300

    Melhorando o layout do Profile

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -2,10 +2,16 @@
   <v-container>
     <v-layout text-xs-left wrap>
       <v-flex xs12>
-        <h1 class="display-3 font-weight-thin">Profile</h1>
-        <h3 class="display-1 font-weight-thin border">Nome: {{user.name}}</h3>
-        <h3 class="display-1 font-weight-thin">Email: {{user.email}}</h3>
-        <h3 class="display-1 font-weight-thin">Descrição: {{user.description}}</h3>
+        <v-card>
+          <v-card-title>
+            <h1 class="display-3 font-weight-thin">Profile</h1>
+          </v-card-title>
+          <v-card-text>
+            <h3 class="display-1 font-weight-thin border">Nome: {{user.name}}</h3>
+            <h3 class="display-1 font-weight-thin">Email: {{user.email}}</h3>
+            <h3 class="display-1 font-weight-thin">Descrição: {{user.description}}</h3>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
