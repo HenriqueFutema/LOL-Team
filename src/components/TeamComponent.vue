70b04635f100f096ad1c54173f2a0b2b0e0549db
HenriqueFutema <minifut0407@gmail.com>
2019-08-22 17:46:09 -0300

    Layout básico TeamComponent

--- src/components/TeamComponent.vue
+++ src/components/TeamComponent.vue
@@ -1,13 +1,42 @@
 <template>
-  <h1>Team</h1>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12 md12>
+        <h1 class="display-3 font-weight-thin">Team</h1>
+      </v-flex>
+      <v-flex xs12 md12>
+        <v-btn block class="font-weight-thin" color="green" dark>Criar Time</v-btn>
+      </v-flex>
+      <v-flex xs12 md9 class="mt-4">
+        <v-text-field label="Nome de um Time" v-model="nameTeam" color="green" dark outlined></v-text-field>
+      </v-flex>
+      <v-flex xs12 md1></v-flex>
+      <v-flex xs12 md2 class="mt-4">
+        <v-form>
+          <v-btn block class="font-weight-thin" color="green" dark>Pesquisar</v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+
+    <v-layout>
+      <oneTeam />
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
+import oneTeam from "./oneTeamComponent";
 
-}
+export default {
+  components: {
+    oneTeam
+  },
+
+  data: () => ({
+    nameTeam: ""
+  })
+};
 </script>
 
 <style>
-
 </style>
